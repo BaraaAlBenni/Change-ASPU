1c23b7d4fedf4c58221548bc184e6febca4ce9bc
BaraaAlBenni <155147209+BaraaAlBenni@users.noreply.github.com>
2025-01-24 12:37:40 +0300

    Update UserApiController.php
    delelte register

--- change-app/app/Http/Controllers/api/UserApiController.php
+++ change-app/app/Http/Controllers/api/UserApiController.php
@@ -17,20 +17,6 @@ use App\Models\CategoryOfUser;
 class UserApiController extends Controller
 {
 
-    public function register(Request $request){
-        $registerUserData = $request->validate([
-            'name'=>'required|string',
-            'email'=>'required|string|email|unique:users',
-            'password'=>'required|min:8'
-        ]);
-        $user = User::create([
-            'name' => $registerUserData['name'],
-            'email' => $registerUserData['email'],
-            'password' => Hash::make($registerUserData['password']),
-        ]);
-        return response()->json([
-            'message' => 'User Created ',
-        ]);
     }
 
 
